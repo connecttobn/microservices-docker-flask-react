398014e919826f6585a1f679bdc538227412c374
nmsbane <madhusudhan638@gmail.com>
2018-05-06 11:29:43 +0530

    add a dockercompose.yml file for composing users service, gave a context for building users service, set up environment variables also enabled volume mounting inside the container so, whenever the code inside the file system changes, code inside the container also changes

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,12 +1,16 @@
 # services/users/project/__init__.py
 
+import os
 from flask import Flask, jsonify
+import sys
+
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
