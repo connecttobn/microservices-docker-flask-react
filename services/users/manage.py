58df15591bf38ae98d3e672d1c263c0b2d81d893
nmsbane <madhusudhan638@gmail.com>
2018-05-10 20:11:59 +0530

    properly tested and developed api for users using TDD, for testing we make use of another database name inside a psql container, like users_test, users_dev the configuration for testing is inside tests/base.py

--- services/users/manage.py
+++ services/users/manage.py
@@ -17,7 +17,6 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
-
 @cli.command()
 def test():
     """ Runs the tests without code coverage"""
@@ -26,6 +25,13 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+@cli.command()
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
