856a0122994503a4dc839e50a8fc765083232b38
nmsbane <madhusudhan638@gmail.com>
2018-05-21 19:55:43 +0530

    introduced server side rendering with template framework, also wrote test cases for it. Learned about b'' and follow_redirects paramters

--- services/users/manage.py
+++ services/users/manage.py
@@ -29,8 +29,8 @@ def test():
 @cli.command()
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(User(username='bane', email="hermanmu@gmail.com"))
+    db.session.add(User(username='awesome bane', email="michael@mherman.org"))
     db.session.commit()
 
 if __name__ == '__main__':
