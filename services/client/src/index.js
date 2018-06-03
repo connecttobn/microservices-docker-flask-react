45fb105d37d2721004024709548a1b6589fefb6b
nmsbane <madhusudhan638@gmail.com>
2018-06-03 11:46:13 +0530

    done with configuring a simple react form with react, now the application is able to store the username and email in the database from react, i.e instead of rendering the page from server side, client ( which is developed using React ) is able to serve the purpose. Also added some unit test cases for testing the snapshot of the UI as well as the form expected output.

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
   constructor() {
     super();
     //this.getUsers();
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: ''
     }
   }
 
@@ -21,12 +24,18 @@ class App extends Component {
             <br/>
             <h1>All Users</h1>
             <hr/><br/>
-              <UsersList users={this.state.users}/>
+              <AddUser addUser={this.addUser.bind(this)}
+                handleChange={this.handleChange.bind(this)}
+                username={this.state.username}
+                email={this.state.email}
+              />
+            <br/>
+            <UsersList users={this.state.users}/>
           </div>
         </div>
       </div>
     )
-  }
+  };
 
   getUsers() {
     console.log(process.env.REACT_APP_USERS_SERVICE_URL);
@@ -38,6 +47,27 @@ class App extends Component {
 
   componentDidMount() {
     this.getUsers();
+  }
+
+  addUser(event) {
+    event.preventDefault();
+    console.log('sanity check!');
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    }
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers();
+        this.setState({username: '', email: ''});
+       })
+      .catch((err) => { console.log(err) })
+  };
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
   }
 
 
